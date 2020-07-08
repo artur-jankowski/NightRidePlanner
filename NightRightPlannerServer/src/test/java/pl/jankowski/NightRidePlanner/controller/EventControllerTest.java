ca56273c4775a780040755a4499cd176ccb78033
Artur Jankowski <artij1@o2.pl>
2020-07-08 11:51:12 +0200

    Implemented happy-path unit tests to the GroupController and UserController test

---
+++ NightRightPlannerServer/src/test/java/pl/jankowski/NightRidePlanner/controller/EventControllerTest.java
@@ -0,0 +1,34 @@
+package pl.jankowski.NightRidePlanner.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventControllerTest {
+
+    @Test
+    void joinEventShouldAddUserToAttendants() {
+
+    }
+
+    @Test
+    void leaveEventShouldRemoveUserFromAttendants() {
+
+    }
+
+    @Test
+    void getAllShouldReturnAllEvents() {
+
+    }
+
+    @Test
+    void updateEventShouldModifyEvent() {
+
+    }
+
+    @Test
+    void getEventShouldReturnEvent() {
+
+    }
+
+}
